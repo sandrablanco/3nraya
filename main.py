ab0af2b0d5b83e9bb22c0b36fabc79b960f7353a
Sandra Blanco <sandra.blanco18@gmail.com>
2026-07-02 13:03:17 +0200

    code computer

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 tablero = [
 [" ", " ", " "],
 [" ", " ", " "],
@@ -16,6 +18,26 @@ mostrar_tablero()
 fila = int(input("Ingrese el número de fila (0-2): "))
 columna = int(input("Ingrese el número de columna (0-2): "))
 if tablero[fila][columna] == " ":
-    tablero[fila][columna] = "X"
+    tablero[fila][columna] = "X" #casilla vacia pon x
 else: 
     print("La posición ya está ocupada. Intente nuevamente.")
+
+while True:
+    # Turno del jugador
+    mostrar_tablero()
+    fila = int(input("Ingrese el número de fila (0-2): "))
+    columna = int(input("Ingrese el número de columna (0-2): "))
+    if tablero[fila][columna] == " ":
+        tablero[fila][columna] = "X"
+    else:
+        print("La posición ya está ocupada. Intente nuevamente.")
+        continue
+    # Turno del ordenador
+    while True:
+        fila_laptop = random.randint(0, 2)
+        columna_laptop = random.randint(0, 2)
+        if tablero[fila_laptop][columna_laptop] == " ":
+            tablero[fila_laptop][columna_laptop] = "O"
+            break
+
+ 
