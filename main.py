20b86c57077b15c9dcd91200bde191d445a4cd7b
Sandra Blanco <sandra.blanco18@gmail.com>
2026-07-02 12:57:07 +0200

    starting game code

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ tablero = [
 ]
 
 def mostrar_tablero():
-    print("-" * 9)
+    print("-" * 9) #linea de separación
     for fila in tablero:
         print(" | ".join(fila))
         print("-" * 9)
 
 mostrar_tablero()
+
+#posicionar ficha del jugador
+fila = int(input("Ingrese el número de fila (0-2): "))
+columna = int(input("Ingrese el número de columna (0-2): "))
+if tablero[fila][columna] == " ":
+    tablero[fila][columna] = "X"
+else: 
+    print("La posición ya está ocupada. Intente nuevamente.")
