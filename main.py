f32a870e9efa63cc535e8fb2de962d89885ce721
Sandra Blanco <sandra.blanco18@gmail.com>
2026-07-02 10:55:04 +0200

    board structure

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ tablero = [
 [" ", " ", " "],
 [" ", " ", " "]
 ]
+
+def mostrar_tablero():
+    print("-" * 9)
+    for fila in tablero:
+        print(" | ".join(fila))
+        print("-" * 9)
+
+mostrar_tablero()
