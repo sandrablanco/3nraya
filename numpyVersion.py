325be1a62b51281dba7689c0d7711c12bb75d1d9
Sandra Blanco <sandra.blanco18@gmail.com>
2026-07-03 17:22:17 +0200

    numpy code in progress

--- numpyVersion.py
+++ numpyVersion.py
@@ -1,11 +1,50 @@
 import numpy as np
 import random
 
-tablero = np.full((3, 3), " ") #tabla de 3x3 
+def create_board():
+    return np.full((3, 3), " ")
+
+board = create_board()
 
 #tablero
 def show_board():
     print("-" * 9) #linea de separación
-    for row in tablero:
+    for row in board:
         print(" | ".join(row))
         print("-" * 9)
+show_board()
+
+def check_winner(board, jugador):
+for row in board:
+    if np.all(row == jugador):
+        return True
+for column in board.T:
+    if np.all(column == jugador):
+        return True
+
+if np.all(np.diag(board) == jugador):
+    return True
+#diagonal a la inversa
+if np.all(np.diag(np.fliplr(board)) == jugador):
+    return True 
+
+def empate(board):
+    return not np.any(board == " ")
+def jugada_usuario(board):
+    while True:
+        try:
+            row = int(input("Fila (0-2): "))
+            column = int(input("Columna (0-2): "))
+            if board[row, column] == " ":
+                board[row, column] = "X"
+                break
+            else:
+                print("Esa casilla ya está ocupada.")
+        except (ValueError, IndexError):
+            print("Entrada inválida, intenta de nuevo.")
+
+
+# if board[row, column] == " ":
+#     board[row, column] = "X"
+# if board[row_laptop, column_laptop] == " ":
+#     board[row_laptop, column_laptop] = "O"
